8d318ccd79f618d581d14bd0df6bec2c52acd77d
Avi Gupta <41137189+avigupta10@users.noreply.github.com>
2021-10-01 12:21:47 +0530

    Fyle backend challenge

--- tests/students_test.py
+++ tests/students_test.py
@@ -1,3 +1,13 @@
+from core.models.assignments import Assignment
+
+
+def test_try_1(client):
+    response = client.get(
+        '/'
+    )
+    assert response.status_code == 200
+
+
 def test_get_assignments_student_1(client, h_student_1):
     response = client.get(
         '/student/assignments',
@@ -42,12 +52,32 @@ def test_post_assignment_student_1(client, h_student_1):
     assert data['teacher_id'] is None
 
 
+def test_post_assignment_update_student_1(client, h_student_1):
+    content = 'ABCD NEW'
+
+    response = client.post(
+        '/student/assignments',
+        headers=h_student_1,
+        json={
+            'id': 5,
+            'content': content
+        })
+
+    assert response.status_code == 200
+
+    data = response.json['data']
+    assert data['content'] == content
+    assert data['state'] == 'DRAFT'
+    assert data['teacher_id'] is None
+
+
 def test_submit_assignment_student_1(client, h_student_1):
+    assignment = Assignment.query.all()
     response = client.post(
         '/student/assignments/submit',
         headers=h_student_1,
         json={
-            'id': 2,
+            'id': len(assignment) - 1,
             'teacher_id': 2
         })
 
@@ -57,3 +87,10 @@ def test_submit_assignment_student_1(client, h_student_1):
     assert data['student_id'] == 1
     assert data['state'] == 'SUBMITTED'
     assert data['teacher_id'] == 2
+
+
+def test_try_2(client):
+    response = client.get(
+        '/client'
+    )
+    assert response.status_code == 404
